256d129605f7b92507f4125021a49a125929a9ee
Jeff Zakrzewski <jeff@zakr.ca>
2025-03-04 11:41:51 -0500

    Update getRunnymedeAlliance() to use 5 second cached value vs. first time valid value

--- src/main/java/frc/robot/RunnymedeUtils.java
+++ src/main/java/frc/robot/RunnymedeUtils.java
@@ -9,11 +9,27 @@ import java.util.Objects;
 
 public class RunnymedeUtils {
 
+  private static final long ALLIANCE_CACHE_TIME_MILLIS = 5000;
   private static DriverStation.Alliance alliance = null;
+  private static long allianceLastUpdated = 0;
 
+  /**
+   * Get the active Alliance. This will return Red if there is no data from the FMS/DriverStation.
+   * It will cache the value for 5 seconds, as when this is constantly used on the robot, it was
+   * occupying close to 1% CPU. It does need to check periodically, as the alliance could
+   * potentially change.
+   *
+   * @return current alliance from FMS/DS, or Red if none set
+   */
   public static DriverStation.Alliance getRunnymedeAlliance() {
-    if (alliance == null) {
-      DriverStation.getAlliance().ifPresent(value -> alliance = value);
+    long currentTime = System.currentTimeMillis();
+    if (alliance == null || currentTime - allianceLastUpdated > ALLIANCE_CACHE_TIME_MILLIS) {
+      DriverStation.getAlliance()
+          .ifPresent(
+              value -> {
+                alliance = value;
+                allianceLastUpdated = currentTime;
+              });
     }
 
     return Objects.requireNonNullElse(alliance, DriverStation.Alliance.Red);
